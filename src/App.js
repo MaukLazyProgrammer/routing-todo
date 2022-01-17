7ffbdb342d383547c4fbe22ecc4d5b7012506213
MaukLazyProgrammer <javapythonscript@gmail.com>
2022-01-17 11:07:08 +0500

    todo Half Done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { GoPencil } from "react-icons/go";
+import { ImCross } from "react-icons/im";
+import { Link } from "react-router-dom";
 
 function App() {
+  const handleListClick = (id) => {
+    console.log(id)
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App container">
+        <div className="col-md-6">
+          <h1>its left div</h1>
+          <form className="form-inline my-4">
+            <input type="text" className="col-md-8 mx-3" name="listName" />
+            <button type="button" className="btn btn-primary">
+              Add
+            </button>
+          </form>
+
+          <table className="table table-borderless table-striped">
+            <thead>
+              <tr>
+                <th>Name</th>
+                <th>Edit</th>
+                <th>Delete</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                  <td><Link to="/ListTodos/1">List 1</Link></td>
+                  <td style={{ color: "blue" }}>{<GoPencil />}</td>
+                  <td style={{ color: "red" }}>{<ImCross />}</td>
+              </tr>
+              <tr>
+                <td>List</td>
+                <td>edit</td>
+                <td>del</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
     </div>
   );
 }
